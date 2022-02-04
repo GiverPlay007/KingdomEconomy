4c391c31ba35314dbadebdf14aba1d6ad88e8920
GiverPlay007 <play.giver.007@gmail.com>
2022-02-03 22:36:11 -0300

    Initialize project

---
+++ src/main/java/me/giverplay/kingdomeconomy/KingdomEconomy.java
@@ -0,0 +1,16 @@
+package me.giverplay.kingdomeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingdomEconomy extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
