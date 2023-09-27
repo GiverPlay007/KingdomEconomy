9025624efba7e929675ddb3c3a506eff3304250b
GiverPlay007 <play.giver.007@gmail.com>
2023-09-27 20:20:29 -0300

    Initialize Forge project

--- src/main/java/me/giverplay/kingdomeconomy/KingdomEconomy.java
+++ src/main/java/me/giverplay/kingdomeconomy/KingdomEconomy.java
@@ -1,16 +1,37 @@
 package me.giverplay.kingdomeconomy;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
-public final class KingdomEconomy extends JavaPlugin {
+@Mod(KingdomEconomy.MOD_ID)
+public class KingdomEconomy {
+  public static final String MOD_ID = "kingdomeconomy";
 
-  @Override
-  public void onEnable() {
+  private static final Logger LOGGER = LogUtils.getLogger();
+
+  public KingdomEconomy() {
+    IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    modEventBus.addListener(this::commonSetup);
+
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  private void commonSetup(final FMLCommonSetupEvent event) {
 
   }
 
-  @Override
-  public void onDisable() {
-
+  @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+  public static class ClientModEvents {
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+    }
   }
 }
